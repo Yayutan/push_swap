0c874cf96367c6a421996db349ce80b7ca8ef016
Yayutan <chuangmic25@gmail.com>
2020-01-31 20:32:47 -0800

    fixed argc cnt

--- srcs/checker.c
+++ srcs/checker.c
@@ -100,7 +100,7 @@ int					main(int ac, char **av)
 	t_ckr			*ckr;
 
 	ckr = set_ckr_structs();
-	if (ac > 2 && !setup_init_st(ckr, ac, av))
+	if (ac > 1 && !setup_init_st(ckr, ac, av))
 	{
 		clean_ckr_structs(ckr);
 		ft_err_exit("Error");
